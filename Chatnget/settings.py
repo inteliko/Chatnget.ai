9926792b29aa82e3f72e3b7f0bfc28c24b2d98c7
riad <maksusbinislam@gmail.com>
2023-05-10 22:19:29 +0600

    CSRF TOKEN

--- Chatnget/settings.py
+++ Chatnget/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_7pgbe2307($p0e^jj#kumhy#0b7!su&-4^bpdd-$tc-efek7s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['*']
 
 #CSRF TOKEN
 
